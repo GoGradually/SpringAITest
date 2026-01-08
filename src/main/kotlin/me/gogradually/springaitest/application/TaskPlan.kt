6112d6c1bea8d588586c83c35a29af50ba349975
GoGradually <chkun3109@gmail.com>
2026-01-08 21:04:50 +0900

    feat: TaskPlan 및 TaskStep 데이터 클래스 추가

---
+++ src/main/kotlin/me/gogradually/springaitest/application/TaskPlan.kt
@@ -0,0 +1,3 @@
+package me.gogradually.springaitest.application
+
+data class TaskPlan(val goal: String, val steps: List<TaskStep>)
---
+++ src/main/kotlin/me/gogradually/springaitest/application/TaskStep.kt
@@ -0,0 +1,3 @@
+package me.gogradually.springaitest.application
+
+data class TaskStep(val stepNumber: Int, val description: String)
